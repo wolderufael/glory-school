f9b0b675279e1b36f5ec8d5705da06d049bcca38
ake144 <tamiratgedi1@gmail.com>
2025-06-20 00:04:15 +0300

    image patterns

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,13 @@ const nextConfig: NextConfig = {
   },
   eslint: {
     ignoreDuringBuilds: true,
+  },
+  images:{
+  remotePatterns:[
+    {
+      hostname:'images.unsplash.com',
+    }
+  ]
   }
 };
 
