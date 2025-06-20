8ba71dd7a627de8b568b02894ef9af8721cc3ecf
wolderufael <wolderuphaelkassahun@gmail.com>
2025-06-20 23:35:38 +0300

    Import fix

--- next.config.ts
+++ next.config.ts
@@ -14,7 +14,19 @@ const nextConfig: NextConfig = {
       hostname:'images.unsplash.com',
     }
   ]
-  }
+  },
+  async rewrites() {
+    const env = process.env.NODE_ENV;
+    return [
+      {
+        source: "/:path*",
+        destination:
+          env === "production"
+            ? "https://168.231.80.15/:path*" // Use HTTPS in production if your backend supports it
+            : "http://168.231.80.15/:path*", // Use HTTP in development
+      },
+    ];
+  },
 };
 
 export default nextConfig;
