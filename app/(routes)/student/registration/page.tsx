afa47a3301957781b7b904c5e651a98d2297513e
ake144 <tamiratgedi1@gmail.com>
2025-07-05 16:21:40 +0300

    multistep form stuff

--- app/(routes)/student/registration/page.tsx
+++ app/(routes)/student/registration/page.tsx
@@ -1,7 +1,12 @@
+'use client'
+
 import MultiStepForm from "@/components/studentForm/multiStepForm"
 
 
 const Registration=()=>{
+    console.log("window", typeof window);
+    console.log("File exists?", typeof File);
+
     return(
         <MultiStepForm   />
     )
