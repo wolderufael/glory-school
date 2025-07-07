244b6bd6322a1a32ccfb620f80c886a7628e55ff
ake144 <tamiratgedi1@gmail.com>
2025-07-07 15:23:58 +0300

    latest working code

--- components/dashboard/top-nav.tsx
+++ components/dashboard/top-nav.tsx
@@ -34,12 +34,7 @@ export function TopNav({
         <SidebarTrigger />
         <div className="flex items-center gap-2">
           <h1 className="text-lg font-medium text-slate-800">{title}</h1>
-       {/*    <Badge
-            variant="secondary"
-            className="bg-green-100 text-green-700 border-green-200"
-          >
-            4
-          </Badge> */}
+
         </div>
       </div>
 
