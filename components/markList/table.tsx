525c1e77233baea75703c82e15eca13fb8dca5ab
wolderufael <wolderuphaelkassahun@gmail.com>
2025-07-07 10:50:56 +0300

    table fix

--- components/markList/table.tsx
+++ components/markList/table.tsx
@@ -223,7 +223,7 @@ const ListTable = () => {
           theoryStatus,
           totalMark,
           theory,
-          gradeInLetter,
+          //gradeInLetter,
           comment,
         };
       }
